295caa41b71725ee6957facd919636bfb4376cc5
liudanyun <yun1297@icloud.com>
2018-01-26 16:11:17 +0800

    修复404跳转错误

--- src/views/common/pages/NotFound.vue
+++ src/views/common/pages/NotFound.vue
@@ -1,45 +1,37 @@
 <template>
 <div id="not-found">
-  <h1 class="nf-title">404</h1>
+  <h1 class="title">404</h1>
   <h2>抱歉，您所访问的页面不存在！</h2>
-  <h3 class="nf-count"> 系统将在
+  <h3 class="content"> 系统将在
     <span class="count">{{ count }}</span>
-    秒后为您返回首页
+    秒后为您跳转
   </h3>
 </div>
 </template>
 <script>
 export default {
   name: 'not-found',
-  data() {
-    return {
-      count: 3
-    }
-  },
+  data: () => ({
+    count: 3
+  }),
   mounted() {
-    this.delayAndRedirect()
-  },
-  methods: {
-    delayAndRedirect() {
-      setInterval(() => {
-        this.count -= 1
-      }, 1000)
-      setTimeout(() => {
-        this.$router.push('/') // 路由跳转首页
-        this.$store.commit('MENU_RESET') // 重置菜单
-      }, 3000)
-    }
+    setInterval(() => {
+      this.count -= 1
+    }, 1000)
+    setTimeout(() => {
+      const url = localStorage.getItem('menuActive') || '/'
+      this.$router.push(url) // 路由跳转
+    }, 3000)
   }
 }
 </script>
 <style lang="scss" scoped>
 #not-found {
-    overflow: hidden;
     text-align: center;
-    .nf-title {
+    .title {
         color: coral;
     }
-    .nf-count {
+    .content {
         color: green;
     }
     .count {
