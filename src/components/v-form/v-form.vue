5541dbe36b7e82a00c01386fcc08a89658cdacde
liudanyun <yun1297@icloud.com>
2018-09-13 11:21:10 +0800

    修改表单组件

--- src/components/v-form/v-form.vue
+++ src/components/v-form/v-form.vue
@@ -1,6 +1,6 @@
 <template>
-<div id="v-form" @keyup.enter.native="handleSubmit('form')">
-  <Form ref="form" :model="model" :rules="rules" :inline="inline" :label-width="labelWidth" :label-position="labelPosition" :style="formWidth">
+<div id="v-form">
+  <Form ref="form" :model="model" :rules="rules" :inline="inline" :label-width="labelWidth" :label-position="labelPosition" :style="formWidth" @keyup.enter.native="handleSubmit('form')">
     <slot name="head"> </slot>
     <FormItem v-for="(item, index) in elem" :key="index" :prop="item.prop" :label="item.label" :label-width="item.labelWidth" :style="item.width">
       <Input v-if="!item.element" :type="item.type" v-model="model[item.prop]" :placeholder="item.placeholder" :size="item.size" :disabled="item.disabled" :icon="item.icon" :number="item.number" :rows="item.rows" :autosize="item.autosize" :style="item.elemWidth"></Input>
